764bcb1b664e183b1bbbaf6f72f0d4af36bae235
Roman Sukhopleshchenko <roman.sukhopleshchenko@yandex.ru>
2022-11-26 12:00:15 +0200

    feat: add new tests for yaml format

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -9,9 +9,16 @@ const __dirname = path.dirname(__filename);
 
 const getFixturePath = (fileName) => path.join(__dirname, '__fixtures__', fileName);
 
-test('genDiff', () => {
+test('genDiff JSON', () => {
   const jsonBefore = getFixturePath('file1.json');
   const jsonAfter = getFixturePath('file2.json');
   const jsonExpectedResult = readFileSync(getFixturePath('result.txt'), 'utf-8');
   expect(genDiff(jsonBefore, jsonAfter)).toEqual(jsonExpectedResult);
+});
+
+test('genDiff YAML', () => {
+  const yamlBefore = getFixturePath('filepath1.yml');
+  const yamlAfter = getFixturePath('filepath2.yml');
+  const yamlExpectedResult = readFileSync(getFixturePath('result.txt'), 'utf-8');
+  expect(genDiff(yamlBefore, yamlAfter)).toEqual(yamlExpectedResult);
 });
