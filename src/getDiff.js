a2cb82d97a8a3a6a5a19f607930ad8227f8e59dd
Roman Sukhopleshchenko <roman.sukhopleshchenko@yandex.ru>
2022-12-04 12:15:07 +0200

    feat: add stylish formatter

--- src/getDiff.js
+++ src/getDiff.js
@@ -1,39 +1,28 @@
 import _ from 'lodash';
 
-const getNodeType = (value1, value2) => {
-  if (_.isObject(value1) && _.isObject(value2)) return 'object';
-  if (_.isEqual(value1, value2)) return 'unchanged';
-  if (_.isUndefined(value1)) return 'added';
-  if (_.isUndefined(value2)) return 'deleted';
-  return 'changed';
+const getDiffTree = (data1, data2) => {
+  const unionKeys = _.sortBy(_.union(Object.keys(data1), Object.keys(data2)));
+
+  return unionKeys.map((key) => {
+    if (!_.has(data1, key) && _.has(data2, key)) {
+      return { type: 'added', key, value: data2[key] };
+    }
+    if (_.has(data1, key) && !_.has(data2, key)) {
+      return { type: 'deleted', key, value: data1[key] };
+    }
+    if (_.isObject(data1[key]) && _.isObject(data2[key])) {
+      const children = getDiffTree(data1[key], data2[key]);
+      return { type: 'object', key, children };
+    }
+    if (_.isEqual(data1[key], data2[key])) {
+      return {
+        type: 'unchanged', key, value: data1[key],
+      };
+    }
+    return {
+      type: 'changed', key, value: data1[key], value2: data2[key],
+    };
+  });
 };
 
-const buildNode = (keyName, nodeType, value, newValue) => ({
-  keyName,
-  nodeType,
-  value,
-  newValue,
-});
-
-const getDifference = (data1, data2) => {
-  const buildNodeByType = {
-    object: (key, value, newValue) => buildNode(key, 'object', getDifference(value, newValue)),
-    unchanged: (key, value) => buildNode(key, 'unchanged', value),
-    added: (key, value, newValue) => buildNode(key, 'added', value, newValue),
-    deleted: (key, value) => buildNode(key, 'deleted', value),
-    changed: (key, value, newValue) => buildNode(key, 'changed', value, newValue),
-  };
-
-  const unionKeys = _.union(_.keys(data1), _.keys(data2));
-  console.log(unionKeys);
-
-  return _.reduce(unionKeys, (arr, key) => {
-    const value = data1[key];
-    const newValue = data2[key];
-    const nodeType = getNodeType(value, newValue);
-    const node = buildNodeByType[nodeType](key, value, newValue);
-    return [...arr, node];
-  }, []);
-};
-
-export default getDifference;
+export default getDiffTree;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import { cwd } from 'node:process';
 import { readFileSync } from 'node:fs';
 import { resolve, extname } from 'node:path';
 import parseFile from './parsers.js';
-import getDifference from './getDiff.js';
+import getDiffTree from './getDiff.js';
+import makeResultInFormat from './formatters/stylish.js';
 
 const getFilepath = (filepath) => resolve(cwd(), filepath);
 
@@ -12,75 +13,16 @@ const readFile = (path) => {
   return { fileText, fileExtention };
 };
 
-/* const getChanges = (obj1, obj2) => {
-  const unionKeys = _.sortBy(_.union(Object.keys(obj1), Object.keys(obj2)));
-
-  const changes = unionKeys.map((key) => {
-    const value1 = obj1[key];
-    const value2 = obj2[key];
-
-    if (_.isEqual(value1, value2)) {
-      return {
-        type: 'unchanged',
-        key,
-        value: value1,
-      };
-    }
-    if (!_.has(obj1, key)) {
-      return {
-        type: 'added',
-        key,
-        value: value2,
-      };
-    }
-    if (!_.has(obj2, key)) {
-      return {
-        type: 'deleted',
-        key,
-        value: value1,
-      };
-    }
-    return {
-      type: 'changed',
-      key,
-      value1,
-      value2,
-    };
-  });
-  return changes;
-};
-*/
-
-const getResult = (array) => {
-  const result = array.map((element) => {
-    const { type } = element;
-    switch (type) {
-      case 'deleted':
-        return `  - ${element.key}: ${element.value}`;
-      case 'unchanged':
-        return `    ${element.key}: ${element.value}`;
-      case 'changed':
-        return `  - ${element.key}: ${element.value1} \n  + ${element.key}: ${element.value2}`;
-      case 'added':
-        return `  + ${element.key}: ${element.value}`;
-      default:
-        throw new Error(`Unknown element type: ${type}`);
-    }
-  });
-  return `{\n${result.join('\n')}\n}`;
-};
-
 const genDiff = (filepath1, filepath2) => {
   const file1 = readFile(getFilepath(filepath1));
   const data1 = parseFile(file1.fileText, file1.fileExtention);
-  console.log(data1);
 
   const file2 = readFile(getFilepath(filepath2));
   const data2 = parseFile(file2.fileText, file2.fileExtention);
 
-  const difference = getDifference(data1, data2);
+  const diffTree = getDiffTree(data1, data2);
 
-  const result = getResult(difference);
+  const result = makeResultInFormat(diffTree);
 
   return result;
 };
